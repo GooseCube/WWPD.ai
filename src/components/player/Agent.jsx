abb5f843b2280d32352302898f7040c7bbdcedb1
todd <thickam@outlook.com>
2024-01-04 21:09:42 -0800

    quick save

---
+++ src/components/player/Agent.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect } from "react";
+
+// Local Modules
+import { handlePlayerMoveEvent } from "./modules/keyPressListener";
+
+// Sprite Styles
+import "./styles/styles.css";
+import { Sprite } from "./styles/Sprite";
+
+function Agent({
+  agent,
+  setAgents,
+  changePlayerControlled,
+  prevPlayerControlled,
+}) {
+  useEffect(() => {
+    // If playerControlled hasn't changed from false to true, return early
+    if (
+      prevPlayerControlled &&
+      prevPlayerControlled.uid === agent.uid &&
+      prevPlayerControlled.playerControlled &&
+      !agent.playerControlled
+    ) {
+      return;
+    }
+
+    const handleKeyPress = (event) => {
+      if (agent.playerControlled) {
+        handlePlayerMoveEvent(agent, setAgents, event.key);
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyPress);
+
+    // Cleanup function to remove the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("keydown", handleKeyPress);
+    };
+  }, [agent, prevPlayerControlled]);
+
+  return (
+    <div className="sprite-container">
+      <Sprite
+        agent={agent}
+        className={`sprite grid-cell ${
+          agent.frame === 0
+            ? `stand-${agent.direction}`
+            : `walk-${agent.direction}-${agent.frame}`
+        }`}
+        onClick={() => changePlayerControlled(agent)}>
+        {agent.playerControlled ? <div className="sprite-arrow"></div> : ""}
+        <div className="sprite-name-container">{agent.name}</div>
+      </Sprite>
+      <div className="sprite-shadow"></div>
+    </div>
+  );
+}
+
+export default Agent;

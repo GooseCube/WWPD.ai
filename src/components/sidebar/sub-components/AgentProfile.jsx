46e37066a56b0332a17590062263d86a7c5cb732
todd <thickam@outlook.com>
2024-01-09 08:38:35 -0800

    resolved vite warning for import statement in AgentProfile.jsx

--- src/components/sidebar/sub-components/AgentProfile.jsx
+++ src/components/sidebar/sub-components/AgentProfile.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 /**
  * Display the current player controlled agents profile:
- * Image, Name, and Personality 
+ * Image, Name, and Personality
  * @param {object} agents
  * @returns the agent profile
  */
@@ -16,23 +16,36 @@ function AgentProfile({ agents }) {
     );
     setAgent(userControlledAgent);
     if (userControlledAgent) {
-      import(`../../../assets/characters/${userControlledAgent.sprite}`)
+      const controlledAgent = userControlledAgent.sprite.replace(".png", "");
+      import(`../../../assets/characters/${controlledAgent}.png`)
         .then((image) => setSprite(image.default))
         .catch((err) => console.error(err));
     }
   }, [agents]);
 
   if (!agent) {
-    return <div className="agent-profile-error">Selected agents you control should be displayed here.</div>
+    return (
+      <div className="agent-profile-error">
+        Selected agents you control should be displayed here.
+      </div>
+    );
   }
 
   return (
     <div className="agent-profile-container border rounded p-2">
-  <h2>{agent.name}</h2>
-  {sprite && <div className="agent-sprite-image" style={{backgroundImage: `url(${sprite})`, width: '32px', height: '32px'}} />}
-  <p>{agent.personality}</p>
-</div>
-
+      <h2>{agent.name}</h2>
+      {sprite && (
+        <div
+          className="agent-sprite-image"
+          style={{
+            backgroundImage: `url(${sprite})`,
+            width: "32px",
+            height: "32px",
+          }}
+        />
+      )}
+      <p>{agent.personality}</p>
+    </div>
   );
 }
 

076d25ed0bb4d37292b273e6f26c938df5b0c8ef
todd <thickam@outlook.com>
2024-01-08 21:37:05 -0800

    updated agent text bubble display animation

--- src/components/agents/sub-components/SpriteTextBubble.jsx
+++ src/components/agents/sub-components/SpriteTextBubble.jsx
@@ -1,26 +1,35 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 
 function SpriteTextBubble({ agent }) {
   const textRef = useRef(null);
+  const [textArray, setTextArray] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
-    if (textRef.current) {
-      const textWidth = textRef.current.offsetWidth;
-      const containerWidth = textRef.current.parentNode.offsetWidth;
-      const animationDuration = (textWidth / containerWidth) * 30; // Adjust the base speed (30s) as needed
-
-      textRef.current.style.animationDuration = `${animationDuration}s`;
+    if (agent.momentResponse) {
+      const words = agent.momentResponse.split(" ");
+      const chunks = [];
+      for (let i = 0; i < words.length; i += 3) {
+        chunks.push(words.slice(i, i + 3).join(" "));
+      }
+      setTextArray(chunks);
     }
   }, [agent.momentResponse]);
 
+  const displayTextMessage = () => {
+    if (currentIndex < textArray.length) {
+      setTimeout(() => {
+        setCurrentIndex(currentIndex + 1);
+      }, 2000); // Change this value to adjust the time between text changes
+    }
+  };
+
+  useEffect(() => {
+    displayTextMessage();
+  }, [currentIndex, textArray]);
+
   return agent.momentResponse ? (
-    <div className="sprite-text-container">
-      {agent.momentResponse && (
-        <div className="scrolling-text" ref={textRef}>
-          {`${agent.name}: ${agent.momentResponse}`}
-        </div>
-      )}
-    </div>
+    <div className="sprite-text-container">{textArray[currentIndex]}</div>
   ) : (
     <div className="sprite-text-container">{`${agent.name}`}</div>
   );

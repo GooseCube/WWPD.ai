2e1e6af119a9c56c4e88a4cfab93ae1b19e764f1
todd <thickam@outlook.com>
2024-01-10 13:48:02 -0800

    updated sidebar styles, added box shadow

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import { Button, Offcanvas } from "react-bootstrap";
 import { ChevronDoubleRight, ChevronDoubleLeft } from "react-bootstrap-icons";
 

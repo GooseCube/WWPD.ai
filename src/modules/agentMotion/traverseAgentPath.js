7b1d0022022b6cc0b0fa99dca3bdf276ed3b26d4
todd <thickam@outlook.com>
2024-01-11 01:06:23 -0800

    quick save

--- src/modules/agentMotion/traverseAgentPath.js
+++ src/modules/agentMotion/traverseAgentPath.js
@@ -6,14 +6,22 @@ const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 /**
  * Using the path obtained from agentPathfinder, moves the
- * agent from current position to final destination 
+ * agent from current position to final destination
  * @param {object} agent, agent.x and agent.y
- * @param {number[{x: number, y:number}]} path 
+ * @param {number[{x: number, y:number}]} path
  */
 export async function traverseAgentPath(agent, path) {
+  const NUMBER_OF_SPRITE_COLUMNS = 3;
   for (let index = 0; index < path.length; ++index) {
-    let newFrame = (agent.frame + 1) % 3;
-    await updateAgent({...agent, x: path[index].x, y: path[index].y, direction: path[index].direction, frame: newFrame});
+    let newDirection = path[index].direction;
+    let newFrame = (agent.frame + 1) % NUMBER_OF_SPRITE_COLUMNS;
+    await updateAgent({
+      ...agent,
+      x: path[index].x,
+      y: path[index].y,
+      direction: newDirection,
+      frame: newFrame,
+    });
     await delay(100);
   }
 }

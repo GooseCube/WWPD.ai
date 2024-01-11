d71254e96b29ff83087dc621cdc3051f97ba4dfa
todd <thickam@outlook.com>
2024-01-10 22:40:52 -0800

    added grid collisions and meeting places

---
+++ src/modules/mapGridPositions/meetingPlaces.js
@@ -0,0 +1,12 @@
+export const meetingPlaces = {
+  treeStump: { x: 16, y: 18 },
+  sushiStand: { x: 30, y: 30 },
+  groceryStore: { x: 51, y: 36 },
+  bizarre: { x: 53, y: 14 },
+  plazaTable: { x: 27, y: 20 },
+  pagodaPatio: { x: 28, y: 7 },
+  bizarreAlley: { x: 69, y: 20 },
+  theFlat: { x: 88, y: 17 },
+  theDeck: { x: 82, y: 25 },
+  bizarreStreet: { x: 49, y: 45 },
+};

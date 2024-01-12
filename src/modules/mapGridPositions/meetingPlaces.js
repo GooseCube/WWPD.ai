5f532294075921ee34bb4106bb75c62985c5f67e
todd <thickam@outlook.com>
2024-01-11 17:19:59 -0800

    updated meetingplaces

--- src/modules/mapGridPositions/meetingPlaces.js
+++ src/modules/mapGridPositions/meetingPlaces.js
@@ -2,14 +2,95 @@
  * Locations for moment conversation meetings
  */
 export const meetingPlaces = {
-  treeStump: { x: 16, y: 18 },
-  sushiStand: { x: 30, y: 30 },
-  groceryStore: { x: 51, y: 36 },
-  bizarre: { x: 53, y: 14 },
-  plazaTable: { x: 27, y: 20 },
-  pagodaPatio: { x: 28, y: 7 },
-  bizarreAlley: { x: 69, y: 20 },
-  theFlat: { x: 88, y: 17 },
-  theDeck: { x: 82, y: 25 },
-  bizarreStreet: { x: 49, y: 45 },
+  pagodaPatio: {
+    primaryAgent: { x: 28, y: 6, direction: "right" },
+    audiencePositions: [
+      { x: 31, y: 5, direction: "left" },
+      { x: 31, y: 7, direction: "left" },
+      { x: 35, y: 5, direction: "left" },
+      { x: 36, y: 7, direction: "left" },
+      { x: 43, y: 7, direction: "left" },
+    ],
+  },
+  groceryStore: {
+    primaryAgent: { x: 52, y: 30, direction: "down" },
+    audiencePositions: [
+      { x: 52, y: 36, direction: "up" },
+      { x: 48, y: 36, direction: "up" },
+      { x: 49, y: 32, direction: "up" },
+      { x: 54, y: 33, direction: "up" },
+      { x: 59, y: 32, direction: "left" },
+      { x: 58, y: 34, direction: "left" },
+      { x: 56, y: 35, direction: "up" },
+    ],
+  },
+  bizarre: {
+    primaryAgent: { x: 53, y: 14 },
+    audiencePositions: [
+      { x: 0, y: 0, direction: "left" },
+      { x: 0, y: 0, direction: "left" },
+      { x: 0, y: 0, direction: "left" },
+    ],
+  },
+  plazaTable: {
+    primaryAgent: { x: 29, y: 18, direction: "down" },
+    audiencePositions: [
+      { x: 22, y: 19, direction: "right" },
+      { x: 23, y: 26, direction: "up" },
+      { x: 27, y: 29, direction: "up" },
+      { x: 30, y: 30, direction: "up" },
+      { x: 33, y: 30, direction: "up" },
+      { x: 39, y: 20, direction: "left" },
+      { x: 39, y: 26, direction: "left" },
+      { x: 27, y: 25, direction: "up" },
+      { x: 33, y: 26, direction: "up" },
+      { x: 32, y: 35, direction: "up" },
+    ],
+  },
+  bizarreAlley: {
+    primaryAgent: { x: 69, y: 20 },
+    audiencePositions: [
+      { x: 0, y: 0, direction: "left" },
+      { x: 0, y: 0, direction: "left" },
+      { x: 0, y: 0, direction: "left" },
+      { x: 0, y: 0, direction: "left" },
+      { x: 0, y: 0, direction: "left" },
+      { x: 0, y: 0, direction: "left" },
+      { x: 0, y: 0, direction: "left" },
+      { x: 0, y: 0, direction: "left" },
+      { x: 0, y: 0, direction: "left" },
+      { x: 0, y: 0, direction: "left" },
+    ],
+  },
+  theFlat: {
+    primaryAgent: { x: 88, y: 17 },
+    audiencePositions: [
+      { x: 0, y: 0, direction: "left" },
+      { x: 0, y: 0, direction: "left" },
+      { x: 0, y: 0, direction: "left" },
+      { x: 0, y: 0, direction: "left" },
+    ],
+  },
+  theDeck: {
+    primaryAgent: { x: 82, y: 25 },
+    audiencePositions: [
+      { x: 0, y: 0, direction: "left" },
+      { x: 0, y: 0, direction: "left" },
+      { x: 0, y: 0, direction: "left" },
+      { x: 0, y: 0, direction: "left" },
+    ],
+  },
+  bizarreStreet: {
+    primaryAgent: { x: 49, y: 45 },
+    audiencePositions: [
+      { x: 0, y: 0, direction: "left" },
+      { x: 0, y: 0, direction: "left" },
+      { x: 0, y: 0, direction: "left" },
+      { x: 0, y: 0, direction: "left" },
+      { x: 0, y: 0, direction: "left" },
+      { x: 0, y: 0, direction: "left" },
+      { x: 0, y: 0, direction: "left" },
+      { x: 0, y: 0, direction: "left" },
+    ],
+  },
 };

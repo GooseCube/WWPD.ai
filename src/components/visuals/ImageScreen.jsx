ba26b9d28bbc81c9d684d6a86e39c500bfd15807
todd <thickam@outlook.com>
2024-01-12 16:39:59 -0800

    added image for projector screen

--- src/components/visuals/ImageScreen.jsx
+++ src/components/visuals/ImageScreen.jsx
@@ -1,6 +1,16 @@
+import React from "react";
+import projectorScreen from "../../assets/art/projectorScreen.svg";
+import gooseCubeMap from "../../assets/gooseCubeMap.png"
+
+import "./styles/styles.css";
 
 function ImageScreen() {
-  return ;
+  return (
+    <div className="image-screen">
+      <img className="screen" src={projectorScreen} alt="Projector Screen" />
+      <img className="overlay" src={gooseCubeMap} alt="Goose Cube Map" />
+    </div>
+  );
 }
 
 export default ImageScreen;

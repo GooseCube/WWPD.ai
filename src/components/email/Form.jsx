1f44bdd63c9d57d8ddc933e2c7ab56c42df02b1d
todd <thickam@outlook.com>
2024-01-13 14:04:57 -0800

    email form check if moments is empty before display of list

--- src/components/email/Form.jsx
+++ src/components/email/Form.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { AuthContext } from "../../firebase/AuthProvider";
 import Draggable from "react-draggable";
+import { XCircleFill } from "react-bootstrap-icons";
 
 const convertISOTimestamp = (timestamp) => {
   return new Date(timestamp).toLocaleString();
@@ -19,40 +20,50 @@ function Form({
   handleSendEmail,
   emailRef,
 }) {
-  const { moments, messages } = useContext(AuthContext);
+  const { moments } = useContext(AuthContext);
 
   return (
     <Draggable defaultPosition={{ x: 700, y: 100 }}>
       <form
         className="email-form"
         onSubmit={(e) => handleSendEmail(e, emailRef.current.value)}>
+        <div className="toolbar">
+          <XCircleFill
+            className="email-icon"
+            onClick={() => setShowEmailForm(!showEmailForm)}
+          />
+        </div>
         {/* Enter Email to Send Message */}
         <label>Email</label>
         <input className="email" type="email" name="to_email" ref={emailRef} />
 
         {/* Select a Moment from list */}
         <label>Moments</label>
-        <select
-          className="moments"
-          name="moments"
-          onChange={(e) => {
-            {
-              const moment = Object.values(moments)[e.target.value];
-              setMoment({
-                timestamp: moment.timestamp,
-                instruction: moment.prompt.instruction,
-                context: moment.prompt.context,
-                question: moment.prompt.question,
-                response: moment.response,
-              });
-            }
-          }}>
-          {Object.values(moments).map((moment, index) => (
-            <option className="moment-option" key={index} value={index}>
-              {convertISOTimestamp(moment.timestamp)} {moment.response}
-            </option>
-          ))}
-        </select>
+        {Object.keys(moments).length > 0 ? (
+          <select
+            className="moments"
+            name="moments"
+            onChange={(e) => {
+              {
+                const moment = Object.values(moments)[e.target.value];
+                setMoment({
+                  timestamp: moment.timestamp,
+                  instruction: moment.prompt.instruction,
+                  context: moment.prompt.context,
+                  question: moment.prompt.question,
+                  response: moment.response,
+                });
+              }
+            }}>
+            {Object.values(moments).map((moment, index) => (
+              <option className="moment-option" key={index} value={index}>
+                {convertISOTimestamp(moment.timestamp)} {moment.response}
+              </option>
+            ))}
+          </select>
+        ) : (
+          <p>Create a 'moment' in order to use the emailer</p>
+        )}
 
         <div className="button-container">
           <input className="input-btn" type="submit" value="Send" />

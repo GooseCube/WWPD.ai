a7131f2e992fdb225c2aeffabf068815878a742b
todd <thickam@outlook.com>
2024-01-12 16:57:08 -0800

    adjusted image overlay for screen

--- src/components/visuals/ImageScreen.jsx
+++ src/components/visuals/ImageScreen.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import projectorScreen from "../../assets/art/projectorScreen.svg";
-import gooseCubeMap from "../../assets/gooseCubeMap.png"
+import pirateShip from "../../assets/art/pirateShip.png"
 
 import "./styles/styles.css";
 
@@ -8,7 +8,7 @@ function ImageScreen() {
   return (
     <div className="image-screen">
       <img className="screen" src={projectorScreen} alt="Projector Screen" />
-      <img className="overlay" src={gooseCubeMap} alt="Goose Cube Map" />
+      <img className="overlay" src={pirateShip} alt="Pirate Ship" />
     </div>
   );
 }

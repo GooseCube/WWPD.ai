64a7338d7792b2922f9de038f656d8d950ee64fa
todd <thickam@outlook.com>
2024-01-13 13:55:46 -0800

    updated email form moment capture from dropdown

--- src/components/email/Email.jsx
+++ src/components/email/Email.jsx
@@ -4,46 +4,45 @@ import Form from "./Form";
 import "./styles/styles.css";
 
 function EmailForm({ showEmailForm, setShowEmailForm }) {
-  const API_KEY = import.meta.env.VITE_EMAILJS_API_KEY;
-  const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
-  const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
-  // This 'form' object needs to be passed down to the child node
-  const form = useRef();
-  const [emailData, setEmailData] = useState(null);
+  const serviceID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
+  const templateID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
+  const publicKey = import.meta.env.VITE_EMAILJS_API_KEY;
+  const [moment, setMoment] = useState({});
+  const emailRef = useRef();
 
-  const sendEmail = async (event) => {
+  const handleSendEmail = async (event, email) => {
     event.preventDefault();
-    const formData = new FormData(event.target);
-
-    for (let pair of formData.entries()) {
-      console.log(pair[0] + ", " + pair[1]);
+    const templateParams = {
+      ...moment,
+      email: email,
     }
 
-    const emailDataString = formData.get("emailData");
-    if (emailDataString) {
-      const data = JSON.parse(emailDataString);
-      console.log("Form Current: ", data);
-    } else {
-      console.log("No moment selected");
-    }
+    console.log("email: ", email)
+    console.log(templateParams);
 
     // try {
-    //   const result = await emailjs.sendForm(
-    //     SERVICE_ID,
-    //     TEMPLATE_ID,
-    //     form.current,
-    //     API_KEY
+    //   const result = await emailjs.send(
+    //     serviceID,
+    //     templateID,
+    //     templateParams,
+    //     publicKey
     //   );
     //   console.log(result.text);
-    //   form.current.reset(); // clear form
     // } catch (error) {
     //   console.log("Email Request Error: ", error);
     // }
+    emailRef.current.value = '';
   };
 
   return (
     showEmailForm && (
-      <Form form={form} sendEmail={sendEmail} setEmailData={setEmailData} />
+      <Form
+        showEmailForm={showEmailForm}
+        setShowEmailForm={setShowEmailForm}
+        setMoment={setMoment}
+        handleSendEmail={handleSendEmail}
+        emailRef={emailRef}
+      />
     )
   );
 }
--- src/components/email/Form.jsx
+++ src/components/email/Form.jsx
@@ -6,39 +6,47 @@ const convertISOTimestamp = (timestamp) => {
   return new Date(timestamp).toLocaleString();
 };
 
-
-
 /**
  *
  * @param {useRef} form, reference
  * @param {function event} sendEmail, handles onsubmit send request
  * @returns
  */
-function Form({ form, sendEmail, emailData, setEmailData }) {
+function Form({
+  showEmailForm,
+  setShowEmailForm,
+  setMoment,
+  handleSendEmail,
+  emailRef,
+}) {
   const { moments, messages } = useContext(AuthContext);
 
   return (
     <Draggable defaultPosition={{ x: 700, y: 100 }}>
-      <form className="email-form" ref={form} onSubmit={handleSubmit}>
+      <form
+        className="email-form"
+        onSubmit={(e) => handleSendEmail(e, emailRef.current.value)}>
+        {/* Enter Email to Send Message */}
         <label>Email</label>
-        <input className="email" type="email" name="to_email" />
+        <input className="email" type="email" name="to_email" ref={emailRef} />
 
+        {/* Select a Moment from list */}
         <label>Moments</label>
-        <input
-          type="hidden"
-          name="emailData"
-          value={JSON.stringify(emailData)}
-        />
-
         <select
           className="moments"
           name="moments"
           onChange={(e) => {
-            const selectedMoment = Object.values(moments)[e.target.value];
-            setEmailData(selectedMoment);
-          }}
-          
-          >
+            {
+              const moment = Object.values(moments)[e.target.value];
+              setMoment({
+                timestamp: moment.timestamp,
+                instruction: moment.prompt.instruction,
+                context: moment.prompt.context,
+                question: moment.prompt.question,
+                response: moment.response,
+              });
+            }
+          }}>
           {Object.values(moments).map((moment, index) => (
             <option className="moment-option" key={index} value={index}>
               {convertISOTimestamp(moment.timestamp)} {moment.response}
@@ -46,7 +54,15 @@ function Form({ form, sendEmail, emailData, setEmailData }) {
           ))}
         </select>
 
-        <input className="input-btn" type="submit" value="Send" />
+        <div className="button-container">
+          <input className="input-btn" type="submit" value="Send" />
+          <button
+            className="clear-form-btn"
+            type="button"
+            onClick={() => (emailRef.current.value = "")}>
+            Clear
+          </button>
+        </div>
       </form>
     </Draggable>
   );

dbcb8e2e035d9e157a7b2ea62813651e06d2c9c0
todd <thickam@outlook.com>
2024-02-05 18:47:37 -0800

    added firebaseSidebar.js file and transfered functions from firebaseDB

---
+++ src/firebase/firebaseSidebar.js
@@ -0,0 +1,34 @@
+import { update, remove, ref, push, set, onValue } from "firebase/database";
+import { database, auth } from "./firebaseConfig";
+
+/**
+ * Retrieve sidebar properties such as AI Model currently selected
+ * @param {useState Setter} setSidebar
+ */
+export const getSidebarProperties = async (setSidebar) => {
+  const userId = auth.currentUser.uid;
+  const sidebarRefs = ref(database, `users/${userId}/sidebar`);
+  onValue(sidebarRefs, (snapshot) => {
+    let sidebar = snapshot.val();
+    if (!sidebar) {
+      sidebar = {};
+    }
+    if (!sidebar.aiModel) {
+      sidebar.aiModel = "Mixtral";
+    }
+    setSidebar(sidebar);
+    console.log("AI Model Set As: ", sidebar);
+  });
+};
+
+/**
+ * Expects that the sidebar properties are already set when
+ * passed for update. If a property is excluded when passed
+ * then it will be removed on update.
+ * @param {object} sidebar
+ */
+export const updateSidebar = async (sidebar) => {
+  const userId = auth.currentUser.uid;
+  const sidebarRef = ref(database, `users/${userId}/sidebar`);
+  update(sidebarRef, sidebar);
+};

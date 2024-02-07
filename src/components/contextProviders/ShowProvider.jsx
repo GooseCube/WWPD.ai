54c695d25f9d4423ba48d3290868eb612a651c35
todd <thickam@outlook.com>
2024-02-06 23:32:16 -0800

    wrapped App with <ShowProvider /> and added switch() to handle changing the boolean state for each prop

--- src/components/contextProviders/ShowProvider.jsx
+++ src/components/contextProviders/ShowProvider.jsx
@@ -1,16 +1,49 @@
-import { createContext, useState, useContext } from 'react';
+/* eslint-disable react/prop-types */
+/**
+To use this boolean flag global context provider:
+
+EXAMPLE:
+
+import { useShow } from './ShowContext';
+
+function SomeComponent() {
+  const { show, dispatch } = useShow();
+
+  const handleClick = () => {
+    dispatch({ type: 'SET_INTERFACE', payload: !show.interface });
+  };
+
+  // ... rest of your code
+}
+*/
+import { createContext, useReducer, useContext } from "react";
 
 const ShowContext = createContext();
 
+const initialState = {
+  interface: true,
+  emailForm: false,
+  agentCards: false,
+};
+
+function showReducer(state, action) {
+  switch (action.type) {
+    case "SET_INTERFACE":
+      return { ...state, interface: action.payload };
+    case "SET_EMAIL_FORM":
+      return { ...state, emailForm: action.payload };
+    case "SET_AGENT_CARDS":
+      return { ...state, agentCards: action.payload };
+    default:
+      throw new Error(`Unknown action: ${action.type}`);
+  }
+}
+
 export function ShowProvider({ children }) {
-  const [show, setShow] = useState({
-    interface: true,
-    emailForm: false,
-    agentCards: false,
-  });
+  const [show, dispatch] = useReducer(showReducer, initialState);
 
   return (
-    <ShowContext.Provider value={{ show, setShow }}>
+    <ShowContext.Provider value={{ show, dispatch }}>
       {children}
     </ShowContext.Provider>
   );
@@ -19,7 +52,7 @@ export function ShowProvider({ children }) {
 export function useShow() {
   const context = useContext(ShowContext);
   if (context === undefined) {
-    throw new Error('useShow must be used within a ShowProvider');
+    throw new Error("useShow must be used within a ShowProvider");
   }
   return context;
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import ReactDOM from "react-dom/client";
 import AuthProvider from "./components/contextProviders/AuthProvider.jsx";
+import { ShowProvider } from "./components/contextProviders/ShowProvider.jsx";
 import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthProvider>
-    <App />
+    <ShowProvider>
+      <App />
+    </ShowProvider>
   </AuthProvider>
 );

1803bb24acf64f6fadf72729cd3f71813e12e80e
todd <thickam@outlook.com>
2024-02-12 19:24:15 -0800

    added sidebar title animation and backing shadow for each letter

--- src/components/sidebar/sub-components/SidebarHeader.jsx
+++ src/components/sidebar/sub-components/SidebarHeader.jsx
@@ -6,39 +6,52 @@ import { delay } from "../../../modules/momentum/speechModules/helperFunctions";
 
 function SidebarHeader({ icon }) {
   const gameTitle = "G . A . M . E .";
-  const animationTitle = ["", "G . ", "A . ", "M .", "E .", ""];
+  const animationTitle = ["", "G", ".", "A", ".", "M", ".", "E", ".", ""];
   const [title, setTitle] = useState(null);
 
-  // Inside your component
+  /**
+   * On render animation for the game title
+   */
   useEffect(() => {
     setTitle([]);
-
     const animateTitle = async () => {
       for (let index = 0; index < animationTitle.length; ++index) {
-        await delay(index * 300); // delay in milliseconds
+        await delay(300);
         setTitle((prevTitle) => [
           ...prevTitle,
           <div
-            className="animate-letter game-title"
+            className="animate-letter"
             key={index}
-            style={{ animationDelay: `${index * 0.3}s` }}>
+            style={{ animationDelay: `${0.3}s` }}>
             {animationTitle[index]}
           </div>,
         ]);
         if (index + 1 >= animationTitle.length) {
-          delay(index * 300);
-          setTitle(null);
+          delay(300);
+          setTitle(
+            <div
+              className="animate-letter"
+              key={index}
+              style={{ animationDelay: `${0.3}s` }}>
+              {gameTitle}
+            </div>
+          );
         }
       }
     };
-
     animateTitle();
   }, []);
 
   return (
     <div className="sidebar-title mx-5 w-75 d-flex justify-content-around align-items-center">
       <Image className="app-icon-img" src={icon} />
-      <p className="game-title pt-3">{title ? title : gameTitle}</p>
+      {title ? (
+        title
+      ) : (
+        <div className="animate-letter" style={{ animationDelay: `${0.3}s` }}>
+          {gameTitle}
+        </div>
+      )}
     </div>
   );
 }

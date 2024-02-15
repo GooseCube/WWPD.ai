30917cc8f0288e447a6a471063136ac636609211
todd <thickam@outlook.com>
2024-02-15 15:37:06 -0800

    removed unnecessary text 2 image URL response modification as return is a base64 URL handled in api call

--- src/components/messages/subComponents/TextInput.jsx
+++ src/components/messages/subComponents/TextInput.jsx
@@ -1,13 +1,11 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
-import axios from "axios";
 import { pushNewMessage } from "../../../firebase/firebaseMessages";
 import { fetchModelResponse } from "../../../modelAPI/fetchModelResponse";
 
 const buildPrompt = (agent, userPrompt) => {
-  const instruction = `Instruction: Answer the following prompt using the given persona.
-  Persona: Your name is ${agent.name} and you are ${agent.age}.
-  You have a career in ${agent.career} and ${agent.personality}.`;
+  const instruction = `Instruction: Answer the following prompt using the given persona and prompt.
+  Persona: Your career is ${agent.career} and persona is ${agent.personality}.`;
   return `${instruction} Prompt: ${userPrompt}`;
 };
 
@@ -23,47 +21,33 @@ function TextInput({ show, dispatch, sidebar, agents }) {
   const handleInput = async (event) => {
     if (event.key === "Enter") {
       event.preventDefault();
+
+      // Flag will remove text input while fetching the call and
+      // display loading animation for user feedback
       dispatch({ type: "SET_IS_LOADING", payload: true });
       const agent = agents.find((a) => a.playerControlled === true);
-      const response = await fetchModelResponse(
-        sidebar.aiModel.title,
-        buildPrompt(agent, userPrompt)
-      );
-      if (response) {
-        // Check if the response is a URL
-        const isUrl = (str) => {
-          try {
-            new URL(str);
-            return true;
-          } catch (_) {
-            return false;
-          }
-        };
 
-        let finalResponse = response;
+      try {
+        const response = await fetchModelResponse(
+          sidebar.aiModel.title,
+          buildPrompt(agent, userPrompt)
+        );
 
-        if (isUrl(response)) {
-          // If the response is a URL, fetch the image and convert it to a Base64 string
-          const imageResponse = await axios.get(response, {
-            responseType: "blob",
-          });
-          const reader = new FileReader();
-          reader.readAsDataURL(imageResponse.data);
-          await new Promise((resolve) => {
-            reader.onloadend = () => {
-              finalResponse = reader.result;
-              resolve();
-            };
-          });
+        // Error handling for response
+        if (!response) {
+          dispatch({ type: "SET_IS_LOADING", payload: false });
+          throw new Error(
+            `Response is undefined for the text input.\nResponse: ${response}`
+          );
         }
 
         // Save to Firebase messages
-        await pushNewMessage(userPrompt, finalResponse, agent);
+        await pushNewMessage(userPrompt, response, agent);
         // Clear the input & reset isLoading
         setUserPrompt("");
         dispatch({ type: "SET_IS_LOADING", payload: false });
-      } else {
-        dispatch({ type: "SET_IS_LOADING", payload: false });
+      } catch (error) {
+        console.error(`Unable to retrieve the AI Model response\n${error}`);
       }
     }
   };
